6f6c61feb7cee5fc87fa0dbddbba1d41f77c8fc8
‘Shelby <sls1881@gmail.com>
2021-02-09 18:03:46 -0800

    I forgot to commit all day

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <div className='title'>
+                The Horny Collections of Horned Creatures
+            </div>
+        )
+    }
+}
